165de73c5242779c2d0fc37a0a0266072c3909fa
Federico Valdiglesias Martel <federicomrtl@gmail.com>
2025-03-11 15:26:54 -0300

    Se agrego el form de login y la capa de negocio para el ingreso del usuario

---
+++ CentroEstetica/FormLogin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CentroEstetica
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCerrarLogin_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnAcceder_Click(object sender, EventArgs e)
+        {
+            string nombreUsuario = txtUsuario.Text;
+            string constraseniaUusario = txtContrasenia.Text;
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
